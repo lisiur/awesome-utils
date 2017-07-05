c4b6fa4f19831c5d9ac71baecd9841e4fd874115
lisiurday <lisiurday@gmail.com>
2017-07-05 10:49:58 +0800

    add whoTouchedMyProp

--- src/debug/index.js
+++ src/debug/index.js
@@ -1,5 +1,9 @@
-function whoTouchedMyProp() {
-	return 'test'
+function whoTouchedMyProp(obj, prop) {
+	obj[`_${prop}`] = obj[prop]
+	Object.defineProperty(obj, prop, {
+		set: function(newProp) {debugger;this[`_${prop}`] = newProp},
+		get: function() {return this[`_${prop}`]}
+	})
 }
 
 export default whoTouchedMyProp
